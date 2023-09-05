7e298e2c8b0a33ba963f9a8480dd86b5086191b7
chyonee <chiwon99881@gmail.com>
2023-09-05 16:52:19 +0900

    Init

---
+++ src/main/java/com/example/catalogservice/entity/Catalog.java
@@ -0,0 +1,32 @@
+package com.example.catalogservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "catalog")
+public class Catalog implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120, unique = true)
+    private String productId;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(nullable = false, updatable = false, insertable = false)
+    private Integer unitPrice;
+
+    @ColumnDefault(value = "CURRENT_TIMESTAMP")
+    private Date createdAt;
+}
