d0cef386b0fc8c16627456bc401d99c405523d93
chyonee <chiwon99881@gmail.com>
2023-09-06 16:57:13 +0900

    2

--- src/main/java/com/example/catalogservice/entity/Catalog.java
+++ src/main/java/com/example/catalogservice/entity/Catalog.java
@@ -1,5 +1,7 @@
 package com.example.catalogservice.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.ColumnDefault;
@@ -9,6 +11,8 @@ import java.util.Date;
 
 @Data
 @Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
 @Table(name = "catalog")
 public class Catalog implements Serializable {
     @Id
--- src/main/java/com/example/catalogservice/vo/ResponseCatalog.java
+++ src/main/java/com/example/catalogservice/vo/ResponseCatalog.java
@@ -1,11 +1,13 @@
 package com.example.catalogservice.vo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseCatalog {
     private String productId;
