e756158afaf3a36d17994e18976d20c883a8e735
chyonee <chiwon99881@gmail.com>
2023-10-05 15:12:25 +0900

    update

---
+++ docker-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker run -d --network ecommerce-network \
+    --name catalog-service \
+    -e "spring.cloud.config.uri=http://config-service:8888" \
+    -e "spring.rabbitmq.host=rabbitmq" \
+    -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" \
+    -e "logging.file=/api-logs/catalogs-ws.log" \
+    cwchoiit/catalog-service:0.0.1
